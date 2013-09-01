42f94f5ba48686a82b2c8d8f852117a9f3cb7219
tsubasa <tsubasa.wp@gmail.com>
2013-09-01 14:57:45 +0800

    Create Project

---
+++ Blocks_Cocos/Blocks_Cocos/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Blocks_Cocos
+//
+//  Created by Tsubasa on 13-9-1.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
