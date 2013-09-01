2ae6b717f1d91e9600f60b2867052e4c640a210f
tsubasa <tsubasa.wp@gmail.com>
2013-09-01 15:17:33 +0800

    Create Project 'Eclosion'

---
+++ Eclosion/Eclosion/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Eclosion
+//
+//  Created by Tsubasa on 13-9-1.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
